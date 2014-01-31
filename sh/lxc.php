ea57f73695aea6cc7103a955c5dd18789703b100
Gregor Kneitschel <gregor.kneitschel@zalando.de>
2014-01-31 14:41:53 +0100

    added start and stop button for container.

--- sh/lxc.php
+++ sh/lxc.php
@@ -1,16 +1,21 @@
 <?php 
 
-exec('lxc-ls --fancy|awk \'{print $1","$2","$3","$4","$5}\'',$result);
-    header('Content-Type: application/json; charset=UTF-8');
+exec('lxc-ls --fancy | awk \'{print $1","$2","$3","$4","$5}\'',$result); 
+   header('Content-Type: application/json; charset=UTF-8');
 
     echo "[";
     $x = 0;
     $max = count($result)-1;
     foreach ($result as $a)
     {    
-        if ($x==0){ $x++; continue;}
-        if ($x==1){ $x++; continue;}
-        echo json_encode( explode(',',$result[$x]) );
+        if ($x<2){ $x++; continue;}
+	$values = explode(',',$result[$x]);
+	if ($values[1] == "RUNNING"){
+		$values [] = "<button onClick=\"javascript:lxc_stop(".explode(',', $result[$x])[0].")\" class=\"btn btn-mini\">Stop</button>";
+	}else if ($values[1] == "STOPPED"){
+		$values [] = "<button onClick=\"javascript:lxc_start(".explode(',', $result[$x])[0].")\" class=\"btn btn-mini\">Start</button>";
+	}
+	echo json_encode($values);
         echo ($x==$max)?'':',';
         unset($result[$x],$a);
         $x++;
